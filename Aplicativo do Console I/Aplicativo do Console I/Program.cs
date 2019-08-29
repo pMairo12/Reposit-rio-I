e7aeba260251c65a5ed8c4c6554dcb36ed1a6831
MAIRO F DE ANDRADE <p.mairo@academico.ifrn.edu.br>
2019-08-29 11:24:59 -0300

    Testando

---
+++ Aplicativo do Console I/Aplicativo do Console I/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicativo_do_Console_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
